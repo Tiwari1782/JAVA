9f737b84573bb26be74b43041ffda71a6628665d
Prakash Tiwari <prakashtiwarie06@gmail.com>
2026-01-16 20:00:52 +0530

    Added some questions and Prime number Code

--- CH5 - LOOPS/Prime.java
+++ CH5 - LOOPS/Prime.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int n = sc.nextInt();
-        
+
+        if (n == 2) {
+            System.out.println("n is Prime");
+        } else {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    isPrime = false; // n is a multiple of i(i not equal to 1 or n)
+                }
+            }
+            if (isPrime == true) {
+                System.out.println(n + " is Prime");
+            } else {
+                System.out.println(n + " is not prime");
+            }
+        }
         sc.close();
     }
 }
