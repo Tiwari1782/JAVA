243ceacd13df72b23e9abf8cf79110c8e81c11b4
Prakash Tiwari <prakashtiwarie06@gmail.com>
2026-01-18 18:35:55 +0530

    Patterns advanced done

---
+++ CH6 - PATTERNS/Part 2/AdvancedPatterns.java
@@ -0,0 +1,142 @@
+import java.util.*;
+
+public class AdvancedPatterns {
+
+    // Hollow Rectangle
+    public static void HollowRectangle() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int totrows = sc.nextInt();
+        System.out.print("Enter cols : ");
+        int totcols = sc.nextInt();
+        for (int i = 1; i <= totrows; i++) {
+            for (int j = 1; j <= totcols; j++) {
+                if (i == 1 || i == totrows || j == 1 || j == totcols) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+    // Inverted and Rotated Half-Pyramid
+    public static void HalfPyramid() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+    // Inverted Half Pyramid with Numbers
+    public static void numpyramid() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+    // Floyd's Triangle
+    public static void floydstriangle() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        int counter = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+    // Zero-One Triangle
+    public static void ZeroOne() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+    // ButterFly Pattern
+    public static void ButterFly() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        // 1st half
+        for (int i = 1; i <= n; i++) {
+            // Stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces - 2*(n-i)
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 2nd Half
+        for (int i = n; i >= 1; i--) {
+            // Stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces - 2*(n-i)
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // HollowRectangle();
+        // HalfPyramid();
+        // numpyramid();
+        // floydstriangle();
+        // ZeroOne();
+        ButterFly();
+        sc.close();
+    }
+}
