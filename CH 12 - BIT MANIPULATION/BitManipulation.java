ff79a015e4ffec6a92869b7212795eded3f9df2b
Prakash Tiwari <prakashtiwarie06@gmail.com>
2026-01-27 21:02:56 +0530

    Bits done

--- CH 12 - BIT MANIPULATION/BitManipulation.java
+++ CH 12 - BIT MANIPULATION/BitManipulation.java
@@ -48,18 +48,46 @@ public class BitManipulation {
         int bitMask = (~0) << i;
         return n & bitMask;
     }
+
     // Check power of 2
-    public static boolean isPowerofTwo(int n){
-        return (n&(n-1)) == 0;
+    public static boolean isPowerofTwo(int n) {
+        return (n & (n - 1)) == 0;
     }
+
+    // Count set bits in a number
+    public static int countSetBits(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) != 0) { // check our LSB
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
+    // Fast Exponentiation
+    public static int fastExpo(int a, int n) {
+        int ans = 1;
+        while (n > 0) {
+            if ((n & 1) != 0) { // check LSB
+                ans *= a;
+            }
+            a *= a;
+            n = n >> 1;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // System.out.print("Enter n : ");
         // int n = sc.nextInt();
         // oddEven(n);
-        System.out.println(getIthBit(10, 3));
-        System.out.println(SetIthbit(10, 3));
-        System.out.println(clearIthbit(10, 3));
+        // System.out.println(getIthBit(10, 3));
+        // System.out.println(SetIthbit(10, 3));
+        // System.out.println(clearIthbit(10, 3));
+        System.out.println(fastExpo(5, 3));
         sc.close();
     }
 }
